33e15039f1f6b47d52894661be54f7d8741d51a4
Peter Verzijl <peter@verzijl.com>
2016-01-08 14:56:31 +0100

    Added Time class that has time and delta time functions. Getter and setter for camera in GameEngineComponent. Added update to physics (NOT DONE).

--- src/com/peterverzijl/softwaresystems/qwirkle/gameengine/GameEngineComponent.java
+++ src/com/peterverzijl/softwaresystems/qwirkle/gameengine/GameEngineComponent.java
@@ -12,36 +12,43 @@ import java.util.List;
 import javax.swing.JFrame;
 
 import com.peterverzijl.softwaresystems.qwirkle.Game;
-import com.peterverzijl.softwaresystems.qwirkle.rendering.RenderComponent;
-import com.peterverzijl.softwaresystems.qwirkle.ui.Screen;
+import com.peterverzijl.softwaresystems.qwirkle.collision.Collider;
+import com.peterverzijl.softwaresystems.qwirkle.graphics.Camera;
+import com.peterverzijl.softwaresystems.qwirkle.graphics.RenderComponent;
+import com.peterverzijl.softwaresystems.qwirkle.graphics.Screen;
 
 /**
- * The game engine component that can be added to the Canvas, 
- * it does rendering, game loop management and input gathering.
+ * The mGame engine component that can be added to the Canvas, 
+ * it does rendering, mGame loop management and input gathering.
  * @author Peter Verzijl
  *
  */
 public class GameEngineComponent extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
-	private static final int WIDTH = 160;
-	private static final int HEIGHT = 120;
+	public static final int WIDTH = 160;
+	public static final int HEIGHT = 120;
 	private static final int SCALE = 5;
 
 	private boolean running;
 	private Thread thread;
 
-	private Game game;
-	private Screen screen;
+	private Game mGame;
+	private Screen mScreen;
 	private static Input input;
 	private BufferedImage img;
 	private int[] pixels;
 	
+	// TODO (peter) : Make this better!!!!
+	// FIXME (peter) : HACK !!!! 
+	static Camera mCamera;
+	
 	/**
-	 * A list of all objects in the game.
+	 * A list of all objects in the mGame.
 	 */
 	public static List<EngineObject> objects = new ArrayList<EngineObject>();
 	public static List<RenderComponent> renderers = new ArrayList<RenderComponent>();
+	public static List<Collider> colliders = new ArrayList<Collider>();
 
 	public GameEngineComponent() {
 		Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
@@ -49,8 +56,8 @@ public class GameEngineComponent extends Canvas implements Runnable {
 		setMinimumSize(size);
 		setMaximumSize(size);
 
-		game = new Game();
-		screen = new Screen(WIDTH, HEIGHT);
+		mGame = new Game();
+		mScreen = new Screen(WIDTH, HEIGHT);
 		input = new Input(SCALE);
 		
 		addKeyListener(input);
@@ -66,8 +73,8 @@ public class GameEngineComponent extends Canvas implements Runnable {
 			return;
 		}
 		
-		// Call start on the game
-		game.start();
+		// Call start on the mGame
+		mGame.start();
 		
 		running = true;
 		thread = new Thread(this);
@@ -75,13 +82,13 @@ public class GameEngineComponent extends Canvas implements Runnable {
 	}
 	
 	/**
-	 * Frames per second the game runs in.
+	 * Frames per second the mGame runs in.
 	 */
 	public static final int FPS = 60;
 	public static final int MS_PER_FRAME = (int) (1000.0f / FPS);
 
 	/**
-	 * The main thread and function that does the game loop.
+	 * The main thread and function that does the mGame loop.
 	 */
 	@Override
 	public void run() {
@@ -92,13 +99,14 @@ public class GameEngineComponent extends Canvas implements Runnable {
 		double lag = 0.0;
 		while (running) {
 			if (System.currentTimeMillis() - last > 1000.0) {
-				// System.out.println(frameCount + " fps");
+				//System.out.println(frameCount + " fps");
 				last = System.currentTimeMillis();
 				frameCount = 0;
 			}
 
 			double currentTime = System.currentTimeMillis();
 			double elapsedTime = currentTime - lastTime;
+			Time.setDeltaTime(elapsedTime);
 			lastTime = currentTime;
 			lag += elapsedTime;
 
@@ -108,14 +116,22 @@ public class GameEngineComponent extends Canvas implements Runnable {
 				tick();
 				lag -= MS_PER_FRAME;
 			}
-
+			
+			updatePhysics();
 			render();
 			frameCount++;
 		}
 	}
 	
 	/**
-	 * Renders the game to the canvas via a buffer strategy.
+	 * Updates all the colliders
+	 */
+	public void updatePhysics() {
+		PhysicsEngine.update(colliders);
+	}
+	
+	/**
+	 * Renders the mGame to the canvas via a buffer strategy.
 	 */
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
@@ -124,10 +140,10 @@ public class GameEngineComponent extends Canvas implements Runnable {
 			return;
 		}
 
-		screen.render(game);
+		mScreen.render(mGame);
 
 		for (int i = 0; i < WIDTH * HEIGHT; i++) {
-			pixels[i] = screen.mPixels[i];
+			pixels[i] = mScreen.mPixels[i];
 		}
 
 		Graphics g = bs.getDrawGraphics();
@@ -137,15 +153,15 @@ public class GameEngineComponent extends Canvas implements Runnable {
 	}
 	
 	/**
-	 * Ticks the game logic.
+	 * Ticks the mGame logic.
 	 */
 	public void tick() {
-		game.tick();
+		mGame.tick();
 		input.tick();
 	}
 
 	/**
-	 * Stops the game and exits from the game loop.
+	 * Stops the mGame and exits from the mGame loop.
 	 */
 	public synchronized void stop() {
 		if (!running) {
@@ -158,5 +174,21 @@ public class GameEngineComponent extends Canvas implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Set the main camera of the game.
+	 * @param camera The camera to set as the main camera.
+	 */
+	public static void setCamera(Camera camera) {
+		mCamera = camera;
+	}
+
+	/**
+	 * Get camera from the game engine component.
+	 * @return The main camera.
+	 */
+	public static Camera getCamera() {
+		return mCamera;
 	}
 }
