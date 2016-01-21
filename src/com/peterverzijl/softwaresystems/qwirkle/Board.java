2a8b1e25ba232b9d52760d143510acc8f844c8e1
Peter Verzijl <peter@verzijl.com>
2016-01-21 15:09:43 +0100

    Added a move string to node function to board.

--- src/com/peterverzijl/softwaresystems/qwirkle/Board.java
+++ src/com/peterverzijl/softwaresystems/qwirkle/Board.java
@@ -1,5 +1,8 @@
 package com.peterverzijl.softwaresystems.qwirkle;
 
+import com.peterverzijl.softwaresystems.qwirkle.Node;
+import com.peterverzijl.softwaresystems.qwirkle.networking.Protocol;
+
 /**
  * The playing field of the Qwirkle game.
  * @author Peter Verzijl
@@ -22,6 +25,21 @@ public class Board {
 	public boolean placeBlock() {
 		
 		return false;
+	}
+
+	/**
+	 * Reads a move string
+	 * @return
+	 */
+	public static Node moveStringToNode(String s) {
+		String[] move = s.split("" + Protocol.Server.Settings.DELIMITER2);
+		String stone = move[0];
+		int x = Integer.parseInt(move[1]);
+		int y = Integer.parseInt(move[2]); 
+		Node n = new Node();
+		n.setPosition(x, y);
+		n.setBlock(Block.getBlockFromCharPair(stone));
+		return n;
 	}
 	
 	
