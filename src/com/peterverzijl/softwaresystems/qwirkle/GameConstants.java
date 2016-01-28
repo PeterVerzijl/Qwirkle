178aa0fb90dfeff5bb3c688f8bbaf8006b8701d5
DennisVinke <mafustow@gmail.com>
2016-01-28 02:57:19 +0100

    JAVADOC

--- src/com/peterverzijl/softwaresystems/qwirkle/GameConstants.java
+++ src/com/peterverzijl/softwaresystems/qwirkle/GameConstants.java
@@ -1,5 +1,9 @@
 package com.peterverzijl.softwaresystems.qwirkle;
-
+/**
+ * Interface that defines gameconstants
+ * @author Peter Verzijl and Dennis Vinke
+ *
+ */
 public interface GameConstants {
 	public static final int UNSET_NODE = -999;
 	public static final int INALID_MOVE = -1;
